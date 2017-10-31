90faaf041097732b33f313a98b59584781a22ff3
Gleb <gleblordex@gmail.com>
2017-10-31 19:04:31 +0300

    lesson 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -159,7 +159,7 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
 fun squareBetweenExists(m: Int, n: Int): Boolean {
-    if (m == 0 && n == 0 ) return false
+    if (m == 0 && n == 0 ) return true
     for (i in minOf(m,n)..maxOf(m,n)) if (i % sqrt(i.toDouble()) == 0.0)  return true
     return false
 }
@@ -194,13 +194,13 @@ fun cos(x: Double, eps: Double): Double = TODO()
  */
 fun revert(n: Int): Int {
     var n1 = n
-    var rev1 = 0
+    var rev1 = 0.toLong()
     while (n1 != 0) {
        val rev = n1 % 10
         rev1 = (rev1 + rev) * 10
         n1 /= 10
     }
-    return rev1 / 10
+    return (rev1 / 10).toInt()
 }
 
 /**
